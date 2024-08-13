df72878072cf1a3725cc153a65b2c63ecfeaf728
parksangryong <parksangryong@Mac-mini-2.local>
2024-08-13 15:34:08 +0900

    컴포넌트 최적화

---
+++ lib/components/long_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class LongButton extends StatelessWidget{
+  final VoidCallback screenName;
+  final String buttonName;
+  final Color color;
+
+
+  const LongButton({super.key, required this.screenName, required this.buttonName, required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        color: color,
+        borderRadius: BorderRadius.circular(30.0),
+        elevation: 5.0,
+        child: MaterialButton(
+          onPressed: screenName,
+          minWidth: 200.0,
+          height: 42.0,
+          child: Text(
+            buttonName,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,3 +1,5 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flash_chat/components/long_button.dart';
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
@@ -11,11 +13,39 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+    animation = ColorTween(begin: Colors.blueGrey, end: Colors.white)
+        .animate(controller);
+
+    controller.forward();
+
+    controller.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation.value,
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -26,63 +56,43 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
               padding: const EdgeInsets.only(right: 30),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children:[
+                children: [
                   Hero(
                     tag: 'logo',
                     child: SizedBox(
-                      height: 60.0,
+                      height: 60,
                       child: Image.asset('images/logo.png'),
                     ),
                   ),
-                  const Text(
-                    'Flash Chat',
-                    style: TextStyle(
-                      fontSize: 45.0,
-                      fontWeight: FontWeight.w900,
+                  SizedBox(
+                      child: DefaultTextStyle(
+                    style: const TextStyle(
+                        fontSize: 45.0,
+                        fontWeight: FontWeight.w900,
+                        color: Colors.black),
+                    child: AnimatedTextKit(
+                      totalRepeatCount: 1,
+                      animatedTexts: [TypewriterAnimatedText('Flash Chat')],
                     ),
-                  ),
+                  )),
                 ],
               ),
             ),
             const SizedBox(
               height: 48.0,
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, LoginScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: const Text(
-                    'Log In',
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, RegistrationScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: const Text(
-                    'Register',
-                  ),
-                ),
-              ),
-            ),
+            LongButton(
+                screenName: () {
+                  Navigator.pushNamed(context, LoginScreen.id);
+                },
+                buttonName: 'Log In',
+                color: Colors.lightBlueAccent),
+            LongButton(
+                screenName: () {
+                  Navigator.pushNamed(context, RegistrationScreen.id);
+                },
+                buttonName: 'Register',
+                color: Colors.blueAccent),
           ],
         ),
       ),
