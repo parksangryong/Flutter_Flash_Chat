af12eb89ef698c8a382a59693f28a2db501d8b56
parksangryong <parksangryong@Mac-mini-2.local>
2024-08-13 10:58:05 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
+import 'package:flash_chat/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const FlashChat());
+
+class FlashChat extends StatelessWidget {
+  const FlashChat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme:const TextTheme(
+          bodyMedium:  TextStyle(color: Colors.black54),
+        ),
+      ),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id : (context) => WelcomeScreen(),
+        ChatScreen.id : (context) => ChatScreen(),
+        LoginScreen.id : (context) => LoginScreen(),
+        RegistrationScreen.id : (context) => RegistrationScreen()
+      },
+    );
+  }
+}
